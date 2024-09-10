bfbb2efcba22aeaa2d46eef18f003c549c4bf14d
Urban <prestofan@gmail.com>
2024-09-10 17:53:08 +0300

    Исправил ошибки, протестировал.

--- module_10_3.py
+++ module_10_3.py
@@ -1,10 +1,8 @@
-"""
-module_10_3
-"""
+#  module_10_3
+
 from threading import Thread, Lock
 from random import randint
 from time import sleep
-
 
 class Bank:
     def __init__(self):
@@ -12,25 +10,33 @@ class Bank:
         self.lock = Lock()
 
     def deposit(self):
-        for _ in range(100):
+        for i in range(100):
+            deposit_ = randint(50, 500)
+            with self.lock:
+                self.balance += deposit_
+                print(f"Пополнение: {deposit_}. Баланс: {self.balance} ", flush=True)
+                sleep(0.001)
+
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
-            deposit_ = randint(50, 500)
-            self.balance += deposit_
-            print(f"Пополнение: {deposit_}. Баланс: {self.balance} ", flush=True)
-            sleep(0.001)
 
     def take(self):
-        for _ in range(100):
+        for i in range(100):
             take_ = randint(50, 500)
-            print(f"Запрос на {take_} ", flush=True)
-            if self.balance >= take_:
-                self.balance -= take_
-                print(f"Снятие: {take_}. Баланс: {self.balance} ", flush=True)
-            else:
-                self.lock.acquire()
-                print("Запрос отклонён, недостаточно средств. ", flush=True)
+            with self.lock:
+                print(f"Запрос на {take_} ", flush=True)
+
+                if self.balance >= take_:
+                    self.balance -= take_
+                    print(f"Снятие: {take_}. Баланс: {self.balance} ", flush=True)
+                    sleep(0.001)
+                    continue
+
+            self.lock.acquire()
+            print("Запрос отклонён, недостаточно средств. ", flush=True)
             sleep(0.001)
+            if self.lock.locked():
+                self.lock.release()
 
 
 bk = Bank()
